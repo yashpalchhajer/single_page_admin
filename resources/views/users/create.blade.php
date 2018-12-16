1e1d7ad6d63bf9d198dbebc34509e23da83f62b5
Yashpal <yashpalchhajer@gmail.com>
2018-12-16 21:35:40 +0530

    branch init

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -57,8 +57,6 @@
             </div>
         </div>
 
-        {{var_dump($roles)}}
-
         <div class="col-xs-12 col-sm-12 col-md-12">
             <div class="form-group">
                 <strong>Role:</strong>
